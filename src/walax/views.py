9eed13920ed3bef7f9953adfdab847bf90a3539d
Matt Barry <matt@hazelmollusk.org>
2021-07-11 08:13:44 -0400

    .

--- src/walax/views.py
+++ src/walax/views.py
@@ -8,9 +8,22 @@ class WalaxModelViewSet(viewsets.ModelViewSet):
     def list(self, request):
         filters = {}
         for k, v in request.GET.items():
-            if k != 'format': filters[k] = v
+            if k in ['format','_limit','_offset']: continue
+            filters[k] = v
+        filters = self.validate_filters(filters)
         self.queryset = self.queryset.filter(**filters)
-        return super().list(self, request)
+        ret = super().list(self, request)
+        # if '_limit' in request.GET:
+        #     limit = int(request.GET['_limit']) \
+        #         if '_limit' in request.GET else 0
+        #     offset = int(request.GET['_offset']) \
+        #         if '_offset' in request.GET else 0
+        #     print (limit, offset)
+        #     ret = ret[offset:offset+limit]
+        return ret
+
+    def validate_filters(self, filters):
+        return filters
 
     @staticmethod
     def for_model(modelo, serializer=None):
